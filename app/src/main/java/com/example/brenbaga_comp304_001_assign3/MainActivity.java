69f8b6ecbccac723e207f3e4e59789a6f9541e66
brentongalien981 <brentongalien981@gmail.com>
2022-02-22 18:02:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brenbaga_comp304_001_assign3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.brenbaga_comp304_001_assign3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
