47c6c887c80c9cb8b5d1854ce17f516cf03b6d37
brentongalien981 <brentongalien981@gmail.com>
2022-02-22 19:35:18 -0500

    Finished MainActivity

---
+++ app/src/main/java/com/example/brenbaga_comp304_001_assign3/LineDrawingActivity.java
@@ -0,0 +1,14 @@
+package com.example.brenbaga_comp304_001_assign3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LineDrawingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_drawing);
+    }
+}
--- app/src/main/java/com/example/brenbaga_comp304_001_assign3/MainActivity.java
+++ app/src/main/java/com/example/brenbaga_comp304_001_assign3/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.brenbaga_comp304_001_assign3;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] exercises = {"Exercise1", "Exercise2", "Exercise3"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the listview.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                exercises);
+
+        // Bind the list view with array adapter
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+
+        // Set list-item event-listener.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Class theClass = null;
+
+                switch (i) {
+                    case 0:
+                        theClass = LineDrawingActivity.class;
+                        break;
+                    default:
+                        return;
+                }
+
+                Intent myIntent = new Intent(MainActivity.this, theClass);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
+
+
+
 }
